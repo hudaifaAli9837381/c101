02dbbbcf45091030c97b33a152df31a283e619c5
hudaifaAli9837381 <97309655+hudaifaAli9837381@users.noreply.github.com>
2022-06-24 18:48:06 +0100

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,11 @@
+class Car(object):
+    def _init_(self,model,name,color):
+        self.color=color
+        self.name=name
+        self.model=model
+
+def start(self):
+    print("start")
+
+def stop(self):
+    print ()
